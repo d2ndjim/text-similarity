10593ae5910301e4326825cffc928e9575f32117
Lekan <jimoridwanlekan@gmail.com>
2023-05-31 20:06:42 +0100

    Finish authentication

---
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -0,0 +1,14 @@
+import "@/styles/globals.css";
+
+export const metadata = {
+  title: "text-similarity",
+  description: "Open source text similarity checker",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <section className="pt-20">{children}</section>;
+}
